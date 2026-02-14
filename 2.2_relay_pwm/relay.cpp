f3066c7f0067e008f725da5cc533557598b4b34e
Lians-coder <rexdfhr@gmail.com>
2026-02-14 11:04:04 +0200

    HW2.2 added

---
+++ 2.2_relay_pwm/relay.cpp
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+
+constexpr uint8_t PIN_IN = 5;
+constexpr uint8_t PIN_NO = 4;
+
+constexpr uint8_t TIMES_TO_MEASURE = 10;
+uint8_t timesMeasured = 0;
+
+constexpr uint32_t PERIOD = 1000;
+uint32_t lastToggle = 0;
+
+constexpr uint32_t debounceRelayT = 2000; // 2 ms
+volatile bool pinNOwasClosed = false;
+volatile uint32_t lastIsrTime = 0;
+
+uint8_t relayState = HIGH;
+uint32_t startT = 0;
+uint32_t endT   = 0;
+uint32_t sumDelayTime = 0;
+
+void IRAM_ATTR isr()
+{
+  uint32_t now = micros();
+  if (now - lastIsrTime > debounceRelayT)
+  {
+    pinNOwasClosed = true;
+    lastIsrTime = now;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);  
+  pinMode(PIN_IN, OUTPUT);
+  digitalWrite(PIN_IN, relayState);
+  pinMode(PIN_NO, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_NO), isr, FALLING);
+}
+
+void loop()
+{
+  uint32_t now = millis();
+  if (now - lastToggle >= PERIOD)
+  {
+    lastToggle = now;
+    relayState = !relayState;
+    if (relayState == LOW)
+    {
+      pinNOwasClosed = false;
+      startT = micros();
+    }
+    digitalWrite(PIN_IN, relayState);
+  }
+
+  if (pinNOwasClosed && relayState == LOW)
+  {
+    pinNOwasClosed = false;
+    endT = micros();
+    uint32_t delayFromInToClosed = endT - startT;
+    if (delayFromInToClosed >= debounceRelayT)
+    {
+      timesMeasured++;
+      sumDelayTime += delayFromInToClosed;
+      Serial.printf("Trial %u. Delay from IN to close NO contact: %lu μs.\n", timesMeasured, delayFromInToClosed);
+
+      if (timesMeasured % TIMES_TO_MEASURE == 0)
+      {
+        Serial.printf("\tAvg on last %u times: %.2f ms\n\n", TIMES_TO_MEASURE, ((float)sumDelayTime / (TIMES_TO_MEASURE * 1000)));
+        sumDelayTime = 0;
+        timesMeasured = 0;
+      }      
+    }
+  }
+}
---
+++ 2.2_relay_pwm/relay.md
@@ -0,0 +1,21 @@
+# 2.2 Passive / Active
+
+## Relay
+
+### Schematic
+
+[KiCad](ttps://github.com/Lians-coder/embedded_HW/blob/main/2.2_relay_pwm/relay.kicad_sch) schematic  
+
+### Code
+
+[relay.cpp](ttps://github.com/Lians-coder/embedded_HW/blob/main/2.2_relay_pwm/relay.cpp)  
+
+### Result
+
+[Video](https://drive.google.com/file/d/1Njm9ZWw4dILWb69QML731a9H8osa-rNd/view?usp=sharing) demonstrating working process and output.  
+
+## PWM (optional)
+
+### Programming PWM
+
+[pwm.cpp](ttps://github.com/Lians-coder/embedded_HW/blob/main/2.2_relay_pwm/relay.cpp)  
