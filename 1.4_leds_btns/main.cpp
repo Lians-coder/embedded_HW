0cddbece540f0178a2937e827ade5d008b9507f8
VladVanyuk <vladislavvanyuk@gmail.com>
2026-02-14 11:47:19 +0200

    lab2 added pin_t

--- 1.4_leds_btns/main.cpp
+++ 1.4_leds_btns/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 
 // gpio
-constexpr uint8_t BOOT_PIN     = 0;
-constexpr uint8_t EXT_PIN      = 5;
-constexpr uint8_t LED_RED_PIN  = 15;
-constexpr uint8_t LED_BLUE_PIN = 16;
+
+typedef pin_t uint8_t;
+
+constexpr pin_t BOOT_PIN     = 0;
+constexpr pin_t EXT_PIN      = 5;
+constexpr pin_t LED_RED_PIN  = 15;
+constexpr pin_t LED_BLUE_PIN = 16;
 
 // time
 constexpr uint32_t DB_DELAY    = 50;
@@ -22,12 +25,12 @@ struct Btn {
   uint32_t lastDBtime;
   uint8_t state;       // DB state
   uint8_t lastState;   // raw state
-  uint8_t pin;
+  pin_t pin;
   bool mode;           // 1 - fast, 0 - slow
   bool activeLow;      // bootBtn is LOW when pressed, extBnt is HIGH when pressed
   
-  Btn (uint8_t p, bool m)
-  : pin(p), mode(m), activeLow(!m), state(LOW), lastState(LOW), lastDBtime(0) {}
+  Btn (pin_t pin, bool mode)
+  : pin(pin), mode(mode), activeLow(!mode), state(LOW), lastState(LOW), lastDBtime(0) {}
 };//__attribute__((packed)); 
 
 Btn bootBtn {BOOT_PIN, 0};
@@ -40,8 +43,8 @@ void btnHandler(Btn&);
 
 void setupInputPins()
 {
-  pinMode(bootBtn.pin, INPUT_PULLUP);  // LOW - pressed
-  pinMode(extBtn.pin, INPUT);          // HIGH - pressed
+  pinMode((uint8_t)bootBtn.pin, INPUT_PULLUP);  // LOW - pressed
+  pinMode((uint8_t)extBtn.pin, INPUT);          // HIGH - pressed
 
   // sizeof(bootBtn);
 }
